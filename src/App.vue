46a02f47c093b58aa0f22347614e98e2d519f4dd
johnson0903 <johnson50701@gmail.com>
2022-10-30 17:02:02 +0800

    使用 Vue Plugin 橋接 tippy.js 套件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const tooltipText = ref("Hello world");
+</script>
 
 <template>
   <h1>Custom Vue.js 3 Plugins</h1>
+  <input type="text" v-model="tooltipText" /> <br />
+  <span
+    >Hover Over Me
+    <ToolTip :text="tooltipText" :options="{ placement: 'right' }"
+  /></span>
 </template>
